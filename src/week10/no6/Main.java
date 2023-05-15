9753d461e0e6590be13e98ceecb534981728ba8f
itpotatoes <lyj4871@kakao.com>
2023-05-15 20:38:19 +0900

    10주차 과제 완료

--- src/week10/no6/Main.java
+++ src/week10/no6/Main.java
@@ -9,21 +9,15 @@ public class Main {
         shape[0] = new Rectangle();
         shape[1] = new Circle();
 
-
         //1~10사이 랜덤숫자
         int x,y;
         x= (int)(Math.random() * 9)+1;
         y= (int)(Math.random() * 9)+1;
-
         shape[0].move(x,y);
-
 
         x= (int)(Math.random() * 9)+1;
         y= (int)(Math.random() * 9)+1;
         shape[1].move(x,y);
-
-
-
 
     }
 }
